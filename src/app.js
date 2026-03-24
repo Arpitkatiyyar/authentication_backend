0baee2d0fb22aa9e1f36ecd2f3f554eb87793aa2
Arpitkatiyyar <arpitkatiyarhome@gmail.com>
2026-03-25 02:18:16 +0530

    some changes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.routes.js'
-
+import cors from 'cors';
 const app=express()
 
 app.use(express.json())
 app.use(cookieParser())
 app.set("trust proxy", 1);
+app.use(cors({
+  origin: "http://localhost:5173", // explicitly allow your frontend
+  credentials: true                // allow cookies/authorization headers
+}));
 
 app.use("/api/auth",authRouter)
 
